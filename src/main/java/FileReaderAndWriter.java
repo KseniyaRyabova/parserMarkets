35785f57a3daf60ec698f41252809785d0a2cfa7
Kseniya <ryabova.ksyusha@gmail.com>
2022-06-05 21:20:03 +0300

    create vimos parser and writer price to file

---
+++ src/main/java/FileReaderAndWriter.java
@@ -0,0 +1,63 @@
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileReaderAndWriter {
+
+    static List<String> listOfNomenclature = new ArrayList<>();
+    static String filename = System.getProperty("user.dir") + "\\src\\main\\resources\\nomenclatures.xlsx";
+
+/*
+    заполнение списка listOfNomenclature значениями номенклатур из файла
+ */
+    public static void setListOfNomenclature(String filename) throws IOException {
+        FileInputStream fileInputStream = new FileInputStream(filename);
+        var workbook = new XSSFWorkbook(fileInputStream);
+        var sheetIterator = workbook.sheetIterator();
+        while (sheetIterator.hasNext()) {
+            Sheet sheet = sheetIterator.next();
+            var rowIterator = sheet.rowIterator();
+            while (rowIterator.hasNext()) {
+                Row row = rowIterator.next();
+                listOfNomenclature.add(row.getCell(1).toString());
+            }
+        }
+        fileInputStream.close();
+        listOfNomenclature.remove(0);
+        System.out.println(listOfNomenclature);
+    }
+
+    /* Получение списка номенклатур в виде ArrayList */
+    public static List<String> getListOfNomenclature() {
+        try {
+            setListOfNomenclature(filename);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return listOfNomenclature;
+    }
+
+    /* запись в файл цен в определенную ячейку определенного столбца.
+    cellNumber - final номер столбца для конкретного магазина;
+    rowNumber - строка таблицы в зависимости от определенной номенклатуры;
+    */
+    public static void priceWriter(String priceValue, int cellNumber, int rowNumber) throws IOException {
+        FileInputStream fileInputStream = new FileInputStream(filename);
+        var workbook = new XSSFWorkbook(fileInputStream);
+        Sheet sheet1 = workbook.getSheetAt(0);
+        Row row = sheet1.getRow(rowNumber);
+        row.createCell(cellNumber).setCellValue(priceValue);
+        FileOutputStream outputStream = new FileOutputStream(new File(filename));
+        workbook.write(outputStream);
+        outputStream.close();
+        fileInputStream.close();
+    }
+}
---
+++ src/main/java/VimosParser.java
@@ -0,0 +1,102 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.jsoup.select.Selector;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class VimosParser {
+    private ArrayList<String> categoriesList = new ArrayList<>();
+    private String categoryUrl = "https://vimos.ru%s";
+    private HashMap<String, String> nomenclatureListWithPrice = new HashMap<>();
+    private final List<String> nomenclatureOfOwner = FileReaderAndWriter.getListOfNomenclature();
+    private final String priceXpath = "//a[contains(@title, '%s')]/parent::div[contains(@class,'product-card__content')]" +
+            "//span[contains(@class,'product-card__price')]/strong";
+
+    //сбор цен и товаров по всему сайту в мапу nomenclatureListWithPrice
+    public void parseNomenclatureList() {
+        String nomenclatureTitle;
+        String priceXpath;
+        for (String categoryUrl : categoriesList) {
+            try {
+                Document listNomenclatureOfCatalogUrl = Jsoup.connect(String.format(this.categoryUrl, categoryUrl)).get();
+                Element bodyPage = listNomenclatureOfCatalogUrl.body();
+                Elements nomenclatures = bodyPage.getElementsByAttribute("title");
+                for (Element nomenclature : nomenclatures) {
+                    String price = null;
+                    nomenclatureTitle = nomenclature.attr("title");
+                    priceXpath = String.format(this.priceXpath, nomenclatureTitle);
+                    try {
+                        price = bodyPage.selectXpath(priceXpath).text();
+                        System.out.println(price);
+                        nomenclatureListWithPrice.put(nomenclatureTitle, price);
+                    } catch (Selector.SelectorParseException ex) {
+                        ex.printStackTrace();
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    // Запись в файл цены, если товар из списка товаров заказчика есть в мапе nomenclatureListWithPrice
+    public void parsePriceByListNomenclature() {
+        for (var nomenclature : nomenclatureOfOwner) {
+            for (Map.Entry<String, String> entry : nomenclatureListWithPrice.entrySet()) {
+                String key = entry.getKey();
+                String value = entry.getValue();
+                System.out.println(nomenclatureOfOwner.indexOf(key));
+                if (nomenclature.equals(key)) {
+                    try {
+                        int cellNumber = 3;
+                        FileReaderAndWriter.priceWriter(value, cellNumber, nomenclatureOfOwner.indexOf(key) + 1);
+                        break;
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println(nomenclature);
+                }
+            }
+        }
+    }
+
+
+    /*
+    Создание списка paths категорий магазина для дальнейшего парсинга
+    */
+    public void createCatalogList() {
+        try {
+            String baseUrl = "https://vimos.ru/catalog";
+            Document vimosCatalogPage = Jsoup.connect(baseUrl).get();
+            Elements categoriesPath = vimosCatalogPage.body()
+                    .getElementsByAttributeValueContaining("href", "/catalog/");
+            for (Element categoryPath : categoriesPath) {
+                categoriesList.add(categoryPath.attr("href"));
+            }
+            System.out.println(categoriesList.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "VimosParser{" +
+                "nomenclatureListWithPrice=" + nomenclatureListWithPrice +
+                "}\n";
+    }
+
+    public static void main(String[] args) {
+        VimosParser vimosParser = new VimosParser();
+        vimosParser.createCatalogList();
+        vimosParser.parseNomenclatureList();
+        vimosParser.parsePriceByListNomenclature();
+    }
+
+}
